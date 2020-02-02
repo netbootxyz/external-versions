27137b0df7dd341858dafd15c521c4190fbd262c
thelamer <ryankuba@gmail.com>
2020-02-02 13:40:24 -0800

    grab dynamic range of versions and do a sanity check before returning the value

--- version.sh
+++ version.sh
@@ -1,3 +1,9 @@
 #!/bin/bash
 set -e
-curl -fsL https://netboot.joyent.com/smartos.ipxe | grep -A 4 'Platform Images' | awk '/space/ {printf $4","}' |sed 's/.$//'
+VERSION=$(curl -fsL https://netboot.joyent.com/smartos.ipxe | awk '/^item --gap Platform Images/{flag=1;next}/^item --gap Options/{flag=0}flag {printf $4","}' |sed 's/.$//')
+# make sure the return has a sane length
+if [[ $(printf "${VERSION}" | wc -c) > 10 ]]; then
+  echo "${VERSION}"
+else
+  exit 1
+fi
