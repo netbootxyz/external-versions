c34470b70b56bd0d4f62661e440759ad6b5ffe3a
Antony Messerli <antony@mes.ser.li>
2021-08-31 21:51:53 -0500

    Add testing and next versions

--- version.sh
+++ version.sh
@@ -1,9 +1,13 @@
 #!/bin/bash
 set -e
-VERSION=$(curl -sfL https://builds.coreos.fedoraproject.org/streams/stable.json | jq -r '.architectures.x86_64.artifacts.metal.release')
+for VERSION in stable testing next; do
+  ITEM=$(curl -sfL https://builds.coreos.fedoraproject.org/streams/${VERSION}.json | jq -r '.architectures.x86_64.artifacts.metal.release')
+  ALL_VERSIONS+=$ITEM-${VERSION},
+done
+
 # make sure the return has a sane version
-while [[ "${VERSION}" =~ ^[0-9]{2}.[0-9]{8}.[0-9]{1}.[0-9]{1} ]]; do
-  echo "${VERSION}"
+while [[ "${ALL_VERSIONS}" =~ ^[0-9]{2}.[0-9]{8}.[0-9]{1}.[0-9]{1} ]]; do
+  echo "${ALL_VERSIONS}" | sed 's/.$//'
   exit 0
 done
 exit 1
