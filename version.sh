cf49fbf74bf60dc5169826abc5ab988157625518
Antony Messerli <antony@mes.ser.li>
2022-10-14 11:33:03 -0500

    Update version.sh

--- version.sh
+++ version.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 set -e
-VERSION=$(curl -sL https://www.ipfire.org/download | awk -F '(x/|/ipfire-)' '/x86_64-full-/ {print $3;exit}')
+VERSION=$(curl -sL https://www.ipfire.org/download | awk -F '(x/|/ipfire-)' '/-x86_64/ {print $3;exit}')
 # make sure the return has a sane version
 while [[ "${VERSION}" =~ ^[0-9].[0-9][0-9]-core* ]]; do
   echo "${VERSION}"
