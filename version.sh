16de2a682a260378406acbbe37e392c45b6954ab
Antony Messerli <antony@mes.ser.li>
2022-10-14 11:27:44 -0500

    Change sha filename to fix updating

--- version.sh
+++ version.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 set -e
-VERSION=$(curl -sfL http://mirrors.kernel.org/archlinux/iso/latest/md5sums.txt | awk -F '(archlinux-|-x86_64.iso)' '/-x86_64.iso/ {print $2;exit}')
+VERSION=$(curl -sfL http://mirrors.kernel.org/archlinux/iso/latest/sha256sums.txt | awk -F '(archlinux-|-x86_64.iso)' '/-x86_64.iso/ {print $2;exit}')
 # make sure the return has a sane version
 while [[ "${VERSION}" =~ ^[0-9]{4}.[0-9]{2}.[0-9]{2}$ ]]; do
   echo "${VERSION}"
