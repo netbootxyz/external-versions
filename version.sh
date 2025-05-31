c39c882f667cd79fc2390cad04b3f4c484da8b8b
Antony Messerli <antony@mes.ser.li>
2025-05-30 23:15:17 -0500

    Add kairos version checker

--- version.sh
+++ version.sh
@@ -1,12 +1,5 @@
 #!/bin/bash
 
 set -e
-VERSION=$(git ls-remote --tags https://github.com/nix-community/nixos-images.git | grep -v assets | grep -v nixos-unstable | awk -F'/' '{print $3}' | sort -r | paste -s -d 'Q' -)
-
-# make sure the return has a sane version
-while [[ "${VERSION}" =~ ^nixos-.* ]]; do
-  echo "${VERSION}"
-  exit 0
-done
-exit 1
-
+VERSION=$(curl --silent "https://api.github.com/repos/kairos-io/kairos/releases/latest" | jq -r .tag_name)
+echo "${VERSION}"
